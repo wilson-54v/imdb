8943b9df7c8080348d96af9a488def4b3dec9bb6
wilsongachanja934@gmail.com <wilsongachanja934@gmail.com>
2023-10-26 13:02:55 +0300

    create the header section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,27 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: ' I am db Clone',
-  description: 'Created by Manu Gachanja',
-}
+import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
+import SearchBox from "@/components/SearchBox";
+import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+      
+          {/* Header */}
+          <Header />
+
+          {/* Navbar */}
+
+          <Navbar />
+
+          {/* SearchBox */}
+
+          <SearchBox />
+
+          {children}
+        
+      </body>
     </html>
-  )
+  );
 }
